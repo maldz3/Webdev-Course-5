78b759cfc4e22a42e64c653b8c60afa88d8445ce
Maliha <maliha_786@hotmail.com>
2018-02-26 11:15:47 -0800

    Improve click functionality

--- Module-1/app.js
+++ Module-1/app.js
@@ -10,15 +10,18 @@ function LunchCheckController($scope) {
 
   $scope.checkCount = function () {
     if (!$scope.dishes || $scope.dishes.length == 0){
-      $scope.dishes = "Please enter data first"
+      $scope.dishes = "Please enter data first";
+      var count = 0;
     } else {
-    var count = ($scope.dishes.match(/,/g) || []).length
+    count = ($scope.dishes.match(/,/g) || []).length
     count += 1
     }
 
-    $scope.message = function (){
+    $scope.getMessage = function (){
       if (count > 3) {
         return "Too much!";
+      } if ($scope.dishes == "Please enter data first" || count == 0) {
+        return "";
       } else {
         return "Enjoy!";
       }
