3794e3f765891fe42cabf98d8d0d50d848e11d22
Maliha <maliha_786@hotmail.com>
2018-02-26 11:49:29 -0800

    Add color to message and input border

--- Module-1/app.js
+++ Module-1/app.js
@@ -7,7 +7,6 @@ angular.module('LunchCheck', [])
 
 LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
-
   $scope.checkCount = function () {
     if (!$scope.dishes || $scope.dishes.length == 0){
       var count = 0;
@@ -18,10 +17,14 @@ function LunchCheckController($scope) {
 
     $scope.getMessage = function (){
       if (count > 3) {
+        $scope.color = 'green';
         return "Too much!";
-      } if (count == 0) {
+      }
+      if (count == 0) {
+        $scope.color = 'red';
         return "Please enter data first";
       } else {
+        $scope.color = 'green';
         return "Enjoy!";
       }
     };
