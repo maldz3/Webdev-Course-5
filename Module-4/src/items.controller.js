ec55d63aec7e34f42cadf26cadfd4dc0f652162d
Maliha <maliha_786@hotmail.com>
2018-06-19 14:28:28 -0700

    categoryShortName fix

--- Module-4/src/items.controller.js
+++ Module-4/src/items.controller.js
@@ -9,16 +9,17 @@ MenuItemsController.$inject = ['$stateParams', 'MenuDataService'];
 function MenuItemsController($stateParams, MenuDataService) {
   var items = this;
 
-  var promise = MenuDataService.getItemsForCategory('categoryShortName');
+  items.getItemsForCategory = function (categoryShortName) {
+    var promise = MenuDataService.getItemsForCategory(categoryShortName);
 
-  promise.then(function (response) {
-    items.items = response.data;
-    console.log(response.data);
-  })
-  .catch(function () {
-    console.log("Something went terribly wrong.");
-  });
-
+    promise.then(function (response) {
+      items.items = response.data;
+      console.log(response.data);
+    })
+    .catch(function () {
+      console.log("Something went terribly wrong.");
+    });
+  }
 }
 
 })();
